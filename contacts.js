576f254978a2fd46206a1f95acb2a39b56c04349
Roman <68850770+Roman27011986@users.noreply.github.com>
2021-06-01 23:52:49 +0300

    compl

--- contacts.js
+++ contacts.js
@@ -14,44 +14,57 @@ function listContacts() {
   
   function getContactById(contactId) {
     fs.readFile(contactsPath, (err, data) => {
-        if(err){
+        if(err) {
             console.log(err)
             return
         }
-       console.log(JSON.parse(data).find(contact => contact.id === JSON.parse(contactId) )) 
+        const contact = JSON.parse(data).find(contact => contact.id === JSON.parse(contactId))
+       console.table(contact ? contact : `no contact by id:${contactId}`) 
       })
   };
   
   function removeContact(contactId) {
     fs.readFile(contactsPath, (err, data) => {
+
       if(err){
           console.log(err)
           return
       }
-    const contactToremove = JSON.parse(data).filter(contact => contact.id !== JSON.parse(contactId))
-    fs.writeFile(contactsPath,JSON.stringify(contactToremove),(err) => {
-      if(err){
+
+    const contactToremove = JSON.parse(data)
+    .filter(contact => contact.id !== JSON.parse(contactId))
+     
+    fs.writeFile(contactsPath,JSON.stringify(contactToremove, null, ' '),(err) => {
+      if(err) {
           console.log(err)
           return
       }
     })
+
     console.table(contactToremove);
+
     })};
 
     function addContact(name, email, phone) {
-      const newContact = {
-        id:  65,
-        name, 
-        email, 
-        phone
-      }
+      
       fs.readFile(contactsPath, (err, data) => {
         if(err) {
-            console.log(err)
-            return
+          console.log(err)
+          return
         }
 
-        fs.writeFile(contactsPath,JSON.stringify([newContact,...JSON.parse(data)]),(err)=>{
+        const contacts = JSON.parse(data)
+
+        const newContact = {
+          id:  contacts.length + 1,
+          name, 
+          email, 
+          phone
+        }
+
+        const allContacts = [newContact,...contacts]
+        console.table(allContacts);
+        fs.writeFile(contactsPath,JSON.stringify(allContacts, null, ' '),(err)=>{
           if(err){
             console.log(err)
             return
